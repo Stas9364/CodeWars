b27d33419c7c338bf642cf4d80be4929ca0a9462
Stas9364 <stas-lisovskii@mail.ru>
2022-03-05 18:36:38 +0300

    changes

--- script.js
+++ script.js
@@ -28,6 +28,3 @@ function createPhoneNumber(numbers){
     return out;
 }
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
-console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
